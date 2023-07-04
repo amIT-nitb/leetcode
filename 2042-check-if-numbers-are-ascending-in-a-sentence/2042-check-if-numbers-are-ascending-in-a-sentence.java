dd6815963169a9eb1afe76c2e6b8175e5d0ad804
amIT <amIT-nitb@users.noreply.github.com>
2023-07-04 12:21:59 +0530

    Time: 3 ms (55.01%), Space: 41.4 MB (42.09%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -1,30 +1,47 @@
 class Solution {
     public boolean areNumbersAscending(String s) {
-            String[] tokens= s.split(" ",0);
-            int curr = -1;
-            int num;
-            for(String index : tokens){
-                if(isNumber(index)){
-                    num = Integer.parseInt(index);
-                if(num > curr){
-                    curr = num;
-                }
-                else{
-                    return false;
-                }
+        
+        int prev = -1;
+        int num;
+        for(String token: s.split(" ")){
+          try{
+              num = Integer.parseInt(token);
+              if(num <= prev){
+                return false;
               }
-            }
-        return true;
+              else
+                  prev = num;
+          }   
+          catch( Exception e){}
+              
+        }   
+          return true; 
     }
+}   
+        //     String[] tokens= s.split(" ",0);
+        //     int curr = -1;
+        //     int num;
+        //     for(String index : tokens){
+        //         if(isNumber(index)){
+        //             num = Integer.parseInt(index);
+        //         if(num > curr){
+        //             curr = num;
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //       }
+        //     }
+        // return true;
+
     
-    private boolean isNumber(String index){
-        // check if string is number
-        try{
-            Integer.parseInt(index);
-            return true;
-        }
-        catch(NumberFormatException e){
-            return false;
-        }
-    }
-}
+//     private boolean isNumber(String index){
+//         // check if string is number
+//         try{
+//             Integer.parseInt(index);
+//             return true;
+//         }
+//         catch(NumberFormatException e){
+//             return false;
+//         }
+//     }
