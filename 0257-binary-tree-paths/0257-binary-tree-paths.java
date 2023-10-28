ab0111aa45070fa9875f5b82624c38ad7f436d08
amIT <amIT-nitb@users.noreply.github.com>
2023-10-28 17:15:12 +0530

    Time: 1 ms (99.94%), Space: 41.6 MB (57.69%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -17,7 +17,6 @@ class Solution {
 
     List<String> list=new ArrayList<>();
     
-    /*
     public List<String> binaryTreePaths(TreeNode root) {        
         if (root != null) {
            dfsFindLeafsAndAddToResult(root, new StringBuilder()); 
@@ -53,23 +52,23 @@ class Solution {
         dfsFindLeafsAndAddToResult(root.left, new StringBuilder(sb.toString()));
         dfsFindLeafsAndAddToResult(root.right, new StringBuilder(sb.toString()));
     }
-*/    
     
-    public List<String> binaryTreePaths(TreeNode root) {
-        binaryTreePaths(root,"");
-        return list;
-    }
-    public void binaryTreePaths(TreeNode root,String str) {
-        if(root==null)return;
+    
+//     public List<String> binaryTreePaths(TreeNode root) {
+//         binaryTreePaths(root,"");
+//         return list;
+//     }
+//     public void binaryTreePaths(TreeNode root,String str) {
+//         if(root==null)return;
 
-        str+=root.val+"->";
+//         str+=root.val+"->";
 
-        binaryTreePaths(root.left,str);
-        binaryTreePaths(root.right,str);
+//         binaryTreePaths(root.left,str);
+//         binaryTreePaths(root.right,str);
 
-        if(root.left==null && root.right==null) list.add(str.substring(0,str.length()-2));
-        return;
-    }
+//         if(root.left==null && root.right==null) list.add(str.substring(0,str.length()-2));
+//         return;
+//     }
     
     
     
